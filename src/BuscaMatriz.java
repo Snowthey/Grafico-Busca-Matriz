45ebdbcfa67a00f75763f376e0731bb67b1adfd8
Matheus <matheus.garcez09@gmail.com>
2024-05-08 15:10:21 -0300

    Apenas Busca Linear funcionando

---
+++ src/BuscaMatriz.java
@@ -0,0 +1,45 @@
+public class BuscaMatriz {
+    public static int BuscaLinear(int[][] matriz){
+        int linhas = matriz.length;
+        int colunas = matriz[0].length;
+        int menorElemento = matriz[0][0];
+
+        int operacoes = 1;
+
+        for(int i = 0; i < linhas; i++){
+            for(int j = 0; j < colunas; j++){
+                if(matriz[i][j] < menorElemento){
+                    menorElemento = matriz[i][j];
+                }
+
+                operacoes++;
+            }
+        }
+
+        return operacoes;
+    }
+
+    public static int EsquinaSuperiorDireita(int[][]matriz){
+        int operacoes = 0;
+        int linha = 0;
+        int coluna = matriz[0].length - 1;
+        int menorElemento = matriz[linha][coluna];
+
+        while(linha < matriz.length && coluna >= 0){
+            if(matriz[linha][coluna] < menorElemento){
+                menorElemento = matriz[linha][coluna];
+            }
+
+            if(coluna > 0 && matriz[linha][coluna - 1] < matriz[linha + 1][coluna]){
+                coluna--;
+            } else{
+                coluna++;
+            }
+
+            operacoes++;
+        }
+        return operacoes;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.data.xy.XYSeries;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BuscaMatriz objBuscaMatriz = new BuscaMatriz();
+        Grafico objGrafico;
+        Random random = new Random();
+
+        XYSeries series = new XYSeries("Complexidade");
+
+        for(int tamanho = 100; tamanho < 1000; tamanho = tamanho + 100){
+            int valor = 1;
+            int[][] matriz = new int[tamanho][tamanho];
+
+            for(int i = 0; i < tamanho; i++){
+                for(int j = 0; j < tamanho; j++){
+                    matriz[i][j] = valor++;
+                }
+            }
+
+            int quantidade = objBuscaMatriz.BuscaLinear(matriz);
+
+            series.add(tamanho, quantidade);
+            System.out.println("Quantidade de buscas:" + quantidade);
+        }
+
+        objGrafico = new Grafico(series);
+        System.out.println("Gráfico gerado com sucesso!");
+    }
+}
